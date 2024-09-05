c73eee1cd39a6cecb154efaa8223b21220a9ad4e
Blalex <54211201+BlackAlexYT@users.noreply.github.com>
2024-09-05 18:44:31 +0300

    Full solution but with random

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import random
+from random import randint
+
 t = int(input())
 for i in range(t):
     x, y, z = map(int, input().split())
-    summa = 0
-    for j in range(z-1):
-        print(j, j)
-        summa += j
-    print(x*z-summa, y*z-summa)
+    summa1 = 0
+    summa2 = 0
+    for j in range(z - 1):
+        a = random.randint(-1 * 10 ** 7, 1 * 10 ** 7)
+        b = random.randint(-1 * 10 ** 7, 1 * 10 ** 7)
+        summa1 += a
+        summa2 += b
+        print(a, b)
+    print(x * z - summa1, y * z - summa2)
